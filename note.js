6d607963b7347da07d19b853de69f2613554b685
Victor Alves <victoralvescabral7@gmail.com>
2024-07-04 05:16:13 -0300

    First Commit

---
+++ note.js
@@ -0,0 +1,13 @@
+/* 
+
+https://www.youtube.com/watch?v=jBmrduvKl5w&list=PLZlA0Gpn_vH_z2fqIg50_POJrUkJgBu7g
+
+"npm init -y" -> To create the package.json file, essential for any Node.js project, and the y is to say yes to all the questions that are asked when you type only "npm init";
+"npm i save-dev typescript" -> To install the typescript inside the devDependencies in the package.json;
+"npx tsc --init" -> In order to use typescript we need the config file for it, so typing this will create that file;
+
+. A bundler for TypeScript is a tool used to package TypeScript code into a format that can be efficiently served to a web browser or executed in a Node.js environment
+
+... more with the video!
+
+*/
